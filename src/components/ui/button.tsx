8d34044cfc1de90ff2c9dd0bdec579ac1b00b137
AshuraDev <akogbetodonald@gmail.com>
2025-01-19 16:06:10 +0100

    Customise shadcn Button component and setup app theming and font

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -9,18 +9,18 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-primary text-primary-foreground hover:bg-primary/90",
+        default: "flex item-center justify-center gap-2 bg-primary text-white rounded-sm",
         destructive:
-          "bg-destructive text-destructive-foreground hover:bg-destructive/90",
+          "bg-destructive text-destructive-foreground rounded-sm",
         outline:
           "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
         secondary:
-          "bg-secondary text-secondary-foreground hover:bg-secondary/80",
-        ghost: "hover:bg-accent hover:text-accent-foreground",
+          "flex item-center justify-center gap-2 border border-gray-200 bg-secondary text-secondary-foreground rounded-sm",
+        ghost: "hover:bg-accent hover:text-accent-foreground rounded-sm",
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
-        default: "h-10 px-4 py-2",
+        default: "h-10 px-4 py-2 uppercase text-xs md:text-sm",
         sm: "h-9 rounded-md px-3",
         lg: "h-11 rounded-md px-8",
         icon: "h-10 w-10",
